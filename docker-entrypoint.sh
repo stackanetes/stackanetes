d0fd3e4cd060d56cddcb7370518145a9244e3073
DTadrzak <daniel.tadrzak@intel.com>
2016-06-07 12:55:54 +0200

    Remove deploy_all.sh Remove unnecessary envs from docker-entrypoint.sh
    Signed-off-by: DTadrzak <daniel.tadrzak@intel.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,29 +15,14 @@ if [[ ! $(kubectl get nodes -o json | grep compute ) ]] ; then
 	exit 1;
 fi
 
-echo Modifying kolla-k8s.conf
-builderip=$(cat /etc/kolla-k8s/kolla-k8s.conf | grep 2181 | awk '{print $3}')
+echo Modifying stackanetes.conf
+builderip=$(cat /etc/stackanetes/stackanetes.conf | grep 2181 | awk '{print $3}')
 
-sed -i "s@#host = $builderip@host = $ZK_HOST@g" /etc/kolla-k8s/kolla-k8s.conf
-sed -i 's@#kubectl_path@kubectl_path@g' /etc/kolla-k8s/kolla-k8s.conf
-sed -i "s@#tag = 1.0.0@tag = $IMAGE_VERSION@g" /etc/kolla-k8s/kolla-k8s.conf
-sed -i "s@#host_interface = eno1@host_interface = $HOST_INTERFACE@g" /etc/kolla-k8s/kolla-k8s.conf
+sed -i "s@#host = $builderip@host = $ZK_HOST@g" /etc/stackanetes/stackanetes.conf
+sed -i 's@#kubectl_path@kubectl_path@g' /etc/stackanetes/stackanetes.conf
+sed -i "s@#tag = 1.0.0@tag = $IMAGE_VERSION@g" /etc/stackanetes/stackanetes.conf
+sed -i "s@#minion_interface_name = eno1@minion_interface_name = $HOST_INTERFACE@g" /etc/stackanetes/stackanetes.conf
 
-echo Modifying Globals.yml
-sed -i "s@network_interface: \"eth0\"@network_interface: \"$NEUTRON_CNI\"@g" /etc/kolla-k8s/globals.yml
-sed -i "s@neutron_external_interface: \"eth0\"@neutron_external_interface: \"$HOST_INTERFACE\"@g" /etc/kolla-k8s/globals.yml
-sed -i "s@enable_horizon: \"no\"@enable_horizon: \"yes\"@g" /etc/kolla-k8s/globals.yml
-
-#Wait for zookeeper to come online on k8s cluster
-while [ true  ]                                                                                                                                                                                
-do                                                                                                                                                                                             
-    sleep 2                                                                                                                                                                                    
-    ZOOKEEPER_ENDPOINTS=$(kubectl describe svc zookeeper |awk '/Endpoint/ {{print $2}}' |head -1)                                                                                      
-    ZOOKEEPER_ENDPOINTS_ARRAY=(${ZOOKEEPER_ENDPOINTS//,/ })                                                                                                                                  
-    if [ ${#ZOOKEEPER_ENDPOINTS_ARRAY[@]} -eq 3 ]; then                                                                                                                                              
-        break                                                                                                                                                                                  
-    fi                                                                                                                                                                                         
-done 
 
 echo Deploying stackanetes
-kolla-k8s --config-dir /etc/kolla-k8s run all --debug
+stackanetes --config-dir /etc/stackanetes run all --debug
